094f814c89dde4ec7a3f955e3424c4d5658526b0
rnnc <rnnc@github.com>
2018-11-25 21:32:07 -0500

    Updates

---
+++ precache-manifest.515dd9f8463533dc76d5c6c46ac6f08f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a67a62f6127a9feada81",
+    "url": "/stopwatch_proofofconcept/static/js/runtime~main.a67a62f6.js"
+  },
+  {
+    "revision": "0950bd0e0d553743a838",
+    "url": "/stopwatch_proofofconcept/static/js/main.0950bd0e.chunk.js"
+  },
+  {
+    "revision": "3f0b22993c54ddcf39f8",
+    "url": "/stopwatch_proofofconcept/static/js/1.3f0b2299.chunk.js"
+  },
+  {
+    "revision": "0950bd0e0d553743a838",
+    "url": "/stopwatch_proofofconcept/static/css/main.d8bff8bd.chunk.css"
+  },
+  {
+    "revision": "0bff4342087d1d40f6112b5b2da498cf",
+    "url": "/stopwatch_proofofconcept/index.html"
+  }
+];
